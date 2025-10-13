0dca67359059c334ccd59892e766878d4ee41af3
Mhd1992 <mohammednser176@gmail.com>
2025-10-13 21:58:35 +0200

    - design root widget and screens inside it

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hungry/features/auth/view/signup_view.dart';
+import 'package:hungry/root.dart';
 import 'package:hungry/splash_view.dart';
 
 import 'features/auth/view/login_view.dart';
@@ -11,14 +12,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'HungryApp',
-
-      home: SignupView(),
+      home: Root(),
     );
   }
 }
---
+++ lib/root.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hungry/core/constants/app_colors.dart';
+import 'package:hungry/features/auth/view/profile_view.dart';
+import 'package:hungry/features/home/views/home_view.dart';
+
+import 'features/cart/views/cart_view.dart';
+import 'features/orderHistory/views/order_history_view.dart';
+
+class Root extends StatefulWidget {
+  const Root({super.key});
+
+  @override
+  State<Root> createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+  late PageController pageController;
+  late List<Widget> screens;
+  int currentScreen = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    pageController = PageController(initialPage: currentScreen);
+    screens = [HomeView(), CartView(), OrderHistoryView(), ProfileView()];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: pageController,
+        children: [screens[currentScreen]],
+      ),
+
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: AppColors.primaryColor,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        padding: EdgeInsets.all(10),
+        child: BottomNavigationBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent, // Set the background color
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey.shade700,
+          onTap: (index) => setState(() {
+            currentScreen = index;
+            pageController.jumpToPage(currentScreen);
+          }),
+          currentIndex: currentScreen,
+
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.cart),
+              label: 'Cart',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.local_restaurant_sharp),
+              label: 'Order History',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.profile_circled),
+              label: 'Profile',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
